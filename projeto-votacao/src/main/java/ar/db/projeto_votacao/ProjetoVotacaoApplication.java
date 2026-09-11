918a1565310783761ac6c8bb35984c6f0e581740
Dutkercz <dutkercz@gmail.com>
2026-09-11 10:26:59 -0300

    chore: initial backend commit, with default database config.

---
+++ projeto-votacao/src/main/java/ar/db/projeto_votacao/ProjetoVotacaoApplication.java
@@ -0,0 +1,13 @@
+package ar.db.projeto_votacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoVotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoVotacaoApplication.class, args);
+	}
+
+}
