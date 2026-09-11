f695e83e49e43a02f9e3af51568f262980af7f04
Dutkercz <dutkercz@gmail.com>
2026-09-11 13:19:32 -0300

    feat: add CPF repository verification when register a new Associado

--- projeto-votacao/src/main/java/ar/db/projeto_votacao/repository/AssociadoRepository.java
+++ projeto-votacao/src/main/java/ar/db/projeto_votacao/repository/AssociadoRepository.java
@@ -4,4 +4,5 @@ import ar.db.projeto_votacao.domain.Associado;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AssociadoRepository extends JpaRepository<Associado, Long> {
+    boolean existsByCpf(String cpf);
 }
--- projeto-votacao/src/main/java/ar/db/projeto_votacao/service/AssociadoService.java
+++ projeto-votacao/src/main/java/ar/db/projeto_votacao/service/AssociadoService.java
@@ -3,9 +3,11 @@ package ar.db.projeto_votacao.service;
 import ar.db.projeto_votacao.domain.Associado;
 import ar.db.projeto_votacao.dto.AssociadoRequestDto;
 import ar.db.projeto_votacao.dto.AssociadoResponseDto;
+import ar.db.projeto_votacao.exception.AssociateAlreadyRegisteredException;
 import ar.db.projeto_votacao.repository.AssociadoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +15,13 @@ public class AssociadoService {
 
     private final AssociadoRepository associadoRepository;
 
+    @Transactional
     public AssociadoResponseDto cadastrarAssociado(AssociadoRequestDto requestDto) {
+        boolean existeByCpf = associadoRepository.existsByCpf(requestDto.cpf());
+        if (existeByCpf) {
+            throw new AssociateAlreadyRegisteredException("Associado já cadastrado");
+        }
+
         Associado associado = new Associado(requestDto.nome(), requestDto.cpf());
         associadoRepository.save(associado);
         return new AssociadoResponseDto(associado);
